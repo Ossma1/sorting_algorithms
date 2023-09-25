1cdefeecccd46005706742a7d9547b7e5d6b938c
Ossma1 <toumi11oussama@gmail.com>
2023-09-25 16:18:25 -0700

    ff

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,35 +8,19 @@
  */
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-    if (*n1 == NULL || n2 == NULL)
-        return;
-
-    listint_t *n1_prev = (*n1)->prev;
-    listint_t *n1_next = (*n1)->next;
-    listint_t *n2_prev = n2->prev;
-    listint_t *n2_next = n2->next;
-
-    if (n1_prev != NULL)
-        n1_prev->next = n2;
-    else
-        *h = n2;
-
-    if (n1_next != NULL)
-        n1_next->prev = n2;
-
-    n2->prev = n1_prev;
-    n2->next = n1_next;
-
-    if (n2_prev != NULL)
-        n2_prev->next = *n1;
-
-    if (n2_next != NULL)
-        n2_next->prev = *n1;
-
-    (*n1)->prev = n2_prev;
-    (*n1)->next = n2_next;
-    *n1 = n2;
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
 }
+
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
  *                       using the insertion sort algorithm.
@@ -46,47 +30,19 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *iter, *insert, *tmp;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *sorted = NULL;  // La liste triée commence vide
-	listint_t *current = *list;  // Nœud en cours de traitement
-
-	while (current != NULL)
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		listint_t *next = current->next;  // Nœud suivant
-
-		// Insérer current dans la liste triée
-		if (sorted == NULL || current->n < sorted->n)
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
 		{
-			current->next = sorted;
-			current->prev = NULL;
-
-			if (sorted != NULL)
-				sorted->prev = current;
-
-			sorted = current;
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
 		}
-		else
-		{
-			listint_t *temp = sorted;
-
-			while (temp->next != NULL && temp->next->n < current->n)
-			{
-				temp = temp->next;
-			}
-
-			current->next = temp->next;
-
-			if (temp->next != NULL)
-				temp->next->prev = current;
-
-			temp->next = current;
-			current->prev = temp;
-		}
-
-		current = next;
 	}
-
-	*list = sorted;  // Met à jour la liste originale
 }
